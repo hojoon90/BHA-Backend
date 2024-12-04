5b31d5c43ef219acddca86f363d61a6cc47ed603
--global <hojoon90@gmail.com>
2024-12-04 17:16:21 +0900

    파일데이터 처리

--- bha-api/src/main/java/com/bupjangsa/controller/FileController.java
+++ bha-api/src/main/java/com/bupjangsa/controller/FileController.java
@@ -1,9 +1,15 @@
 package com.bupjangsa.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1/file")
 public class FileController {
+
+    @DeleteMapping
+    public void deleteFileData(){
+
+    }
 }
--- bha-api/src/main/java/com/bupjangsa/dto/request/FileRequest.java
+++ bha-api/src/main/java/com/bupjangsa/dto/request/FileRequest.java
@@ -1,2 +1,25 @@
-package com.bupjangsa.dto.request;public class FileRequest {
+package com.bupjangsa.dto.request;
+
+import com.bupjangsa.type.BoardType;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class FileRequest {
+
+    @Getter
+    @Builder
+    public static class FileDeleteRequest{
+        private long id;
+        private long postId;
+        private BoardType boardType;
+        private String originName;
+        private String saveName;
+        private long fileSize;
+    }
+
+
+
 }
