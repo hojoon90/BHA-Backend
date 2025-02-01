c2d9dd22b2c9d327c0c3a610946ae2e9c599ef9c
--global <hojoon90@gmail.com>
2025-02-01 22:47:53 +0900

    회원가입 페이지

--- bha-frontend/src/app/regist/page.js
+++ bha-frontend/src/app/regist/page.js
@@ -1,35 +1,75 @@
 "use client"
 
-import Link from 'next/link';
-import LoginContents from '@/components/LoginContents';
-import URL from '@/data/url';
 import { useState } from 'react';
 
-export default function SiteLogin() {
-    const [userInfo, setUserInfo] = useState(null);
+const RegistrationContents = ({ onChangeRegistration }) => {
+    const [registrationInfo, setRegistrationInfo] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
 
-    const onChangeLogin = (user) => {
-        setUserInfo(user);
-    }
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setRegistrationInfo(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Add validation and registration logic here
+        onChangeRegistration(registrationInfo);
+    };
 
     return (
         <div className="container">
             <div className="c_wrap">
-                {/* <!-- Location --> */}
-                <div className="location">
-                    <ul>
-                        <li><Link href={URL.HOME} className="home" >Home</Link></li>
-                        <li>로그인</li>
-                    </ul>
-                </div>
-                {/* <!--// Location --> */}
 
-                <div className="layout">
-                    <LoginContents
-                        onChangeLogin={onChangeLogin}
-                    />
-                </div>
+                <form onSubmit={handleSubmit}>
+
+                    <div className="layout">
+                        <div className="contents BOARD_CREATE_REG" id="contents">
+
+                            <div className="board_view2">
+                                <dl>
+                                    <dt><label htmlFor="username">아이디:</label></dt>
+                                    <dd>
+                                        <input className="f_input2 w_full" type="text" id="username" name="username" value={registrationInfo.username}
+                                               onChange={handleInputChange} required/>
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt><label htmlFor="password">패스워드:</label></dt>
+                                    <dd>
+                                        <input className="f_input2 w_full" type="password" id="password" name="password" value={registrationInfo.password}
+                                               onChange={handleInputChange} required/>
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt><label htmlFor="confirmPassword">패스워드 확인:</label></dt>
+                                    <dd>
+                                        <input className="f_input2 w_full" type="password" id="confirmPassword" name="confirmPassword"
+                                               value={registrationInfo.confirmPassword} onChange={handleInputChange} required/>
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt><label htmlFor="email">이메일:</label></dt>
+                                    <dd>
+                                        <input className="f_input2 w_full" type="email" id="email" name="email" value={registrationInfo.email}
+                                               onChange={handleInputChange} required/>
+                                    </dd>
+                                </dl>
+                                <button type="submit">Register</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     );
-}
+};
+
+export default RegistrationContents;
