90c5a4e89a3df3c7d5311a298ac8791d1664f2e4
--global <hojoon90@gmail.com>
2025-02-18 15:01:35 +0900

    첨부파일 처리 일정 추가

--- bha-infra/src/main/java/com/bupjangsa/config/BhaSecurityConfig.java
+++ bha-infra/src/main/java/com/bupjangsa/config/BhaSecurityConfig.java
@@ -88,7 +88,7 @@ public class BhaSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+//        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization", "Refresh-Token"));
